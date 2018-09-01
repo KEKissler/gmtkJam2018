ca67b672f200647286140402b7f197d561543eb0
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2018-08-31 17:16:46 -0700

    Add Tilemaps

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SoundCreationController))]
+public class PlayerMovementController : MonoBehaviour {
+    public float speed = 1.0f;
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.velocity = (input * speed * Time.deltaTime);
+	}
+}
