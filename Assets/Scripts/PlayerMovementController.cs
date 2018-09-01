260e0904afdd71bcca500934ca6be63d965eaea4
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2018-08-31 21:40:57 -0700

    Add navmesh/agent/obstacle example

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(SoundCreationController))]
 public class PlayerMovementController : MonoBehaviour {
     public float speed = 1.0f;
-    private Rigidbody2D rb;
+    private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-        rb = transform.GetComponent<Rigidbody2D>();
+        rb = transform.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         rb.velocity = (input * speed * Time.deltaTime);
 	}
 }
---
+++ Assets/Scripts/testNavigation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class testNavigation : MonoBehaviour {
+    private NavMeshAgent nma;
+    public GameObject objToFollow;
+	// Use this for initialization
+	void Start () {
+        nma = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nma.destination = objToFollow.transform.position;
+	}
+}
