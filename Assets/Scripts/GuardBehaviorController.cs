e5b4f851a1a6becbd58dddd3fe68219ff9e3af62
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2018-09-01 15:17:10 -0700

    Add GuardVisionController and early GuardBehaviorController

---
+++ Assets/Scripts/GuardBehaviorController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(GuardVisionController))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class GuardBehaviorController : MonoBehaviour {
+    private enum State{normal, alerted};
+    private State state = State.normal;
+    private GameObject aggroTarget = null;
+    private GuardVisionController vc;
+    private NavMeshAgent nma;
+	// Use this for initialization
+	void Start () {
+        vc = GetComponent<GuardVisionController>();
+        nma = GetComponent<NavMeshAgent>();
+	}
+	
+    public void aggro(GameObject source, bool isSound)
+    {
+        if(isSound || aggroTarget == null)
+        {
+            aggroTarget = source;
+            state = State.alerted;
+            nma.destination = source.transform.position;
+        }
+    }
+
+    public void deaggro(GameObject source)
+    {
+        if (source && source == aggroTarget)
+        {
+            aggroTarget = null;
+            state = State.normal;
+            continuePatrol();
+        }
+    }
+
+    //calculates the next patrolpoint to target based on what is closest when this fxn is called
+    private void continuePatrol()
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if(state == State.normal)
+        {
+            
+            //patrol things
+        }
+	}
+}
---
+++ Assets/Scripts/GuardVisionController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GuardBehaviorController))]
+public class GuardVisionController : MonoBehaviour
+{
+    public float visionConeAngle = 15;
+    public float maxVisionDistance = 15;
+    public LayerMask mask;// make sure all things with colliders in the active list are on a layer specified in the LayerMask
+    public GameObject activeListParent;// a parent of gameobjects to keep track of vision on
+    private GuardBehaviorController gb;
+    private List<Transform> activeList;
+
+    // Use this for initialization
+    void Start()
+    {
+        gb = transform.GetComponent<GuardBehaviorController>();
+        foreach(Transform t in activeListParent.transform)
+        {
+            activeList.Add(t);
+        }
+    }
+
+    public bool CheckLOS(Transform other)
+    {
+        Vector3 hereToOther = other.position - transform.position;
+        //within vision cone
+        if(hereToOther.magnitude <= maxVisionDistance && Vector3.Angle(hereToOther, transform.forward) <= visionConeAngle)
+        {
+            //do a raycast to see if the vision towards target is unobstructed
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, hereToOther, out hit, maxVisionDistance, mask, QueryTriggerInteraction.UseGlobal))
+            {
+                Debug.Log("Raycast hit: " + (hit.collider.name));
+                return (hit.collider.name == "Buddy");
+            }
+        }
+        
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform t in activeList)
+        {
+            if (CheckLOS(t))
+            {
+                gb.aggro(t.gameObject, false);
+            }
+        }
+    }
+}
