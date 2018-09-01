69e108982c540f6c61638dcb3271a1de4a783590
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2018-08-31 23:21:56 -0700

    cleaned up scene for night.

---
+++ Assets/Scripts/tilemapColliderInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class tilemapColliderInitializer : MonoBehaviour
+{
+    private Tilemap walls;
+    public GameObject obstacle;
+    // Use this for initialization
+    void Start()
+    {
+        walls = GetComponent<Tilemap>();
+        BoundsInt bounds = walls.cellBounds;
+        TileBase[] allTiles = walls.GetTilesBlock(bounds);
+        for (int x = 0; x < bounds.size.x; ++x)
+        {
+            for (int y = 0; y < bounds.size.y; ++y)
+            {
+                Tile tile = (Tile)allTiles[x + y * bounds.size.x];
+                if (tile)
+                {
+                    Debug.Log(new Vector2(x, y).ToString() + "\n" + tile.name);
+                }
+            }
+        }
+        //Instantiate(obstacle, walls.GetCellCenterWorld(t.transform));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
